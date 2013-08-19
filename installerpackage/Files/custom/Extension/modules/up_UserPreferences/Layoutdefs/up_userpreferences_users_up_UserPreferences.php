350105834dbe3910ea2667d8c349a3a8826213ac
dbergeron <dbergeron@sugarcrm.com>
2013-08-19 08:38:35 -0600

    Changed the Detail view subpanel buttons

---
+++ installerpackage/Files/custom/Extension/modules/up_UserPreferences/Layoutdefs/up_userpreferences_users_up_UserPreferences.php
@@ -0,0 +1,19 @@
+<?php
+ // created: 2013-08-14 08:16:46
+$layout_defs["up_UserPreferences"]["subpanel_setup"]['up_userpreferences_users'] = array (
+  'order' => 100,
+  'module' => 'Users',
+  'subpanel_name' => 'default',
+  'sort_order' => 'asc',
+  'sort_by' => 'id',
+  'title_key' => 'LBL_UP_USERPREFERENCES_USERS_FROM_USERS_TITLE',
+  'get_subpanel_data' => 'up_userpreferences_users',
+  'top_buttons' => 
+  array (
+    0 => 
+    array (
+      'widget_class' => 'SubPanelTopSelectButton',
+      'mode' => 'MultiSelect',
+    ),
+  ),
+);
