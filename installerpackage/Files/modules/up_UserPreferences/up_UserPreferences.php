ec65ebd50675acfa1298db92b54ef95b175d6811
dbergeron <dbergeron@sugarcrm.com>
2013-08-23 09:41:42 -0600

    update to files again

--- installerpackage/Files/modules/up_UserPreferences/up_UserPreferences.php
+++ installerpackage/Files/modules/up_UserPreferences/up_UserPreferences.php
@@ -17,12 +17,23 @@
  */
 require_once 'modules/up_UserPreferences/up_UserPreferences_sugar.php' ;
 
+/**
+ * User Preferences Mass Update class
+ * @author dbergeron
+ *
+ */
 class up_UserPreferences extends up_UserPreferences_sugar {
 
 	public function __construct(){
 		parent::__construct();
 	}
 	
+	/**
+	 * Applies the current user preferences template to the current user logging in.
+	 * This method is called from the Users logic hook on after_login event
+	 * 
+	 * @param object $userObj
+	 */
 	public function applyUserPreferences($userObj) {
 	    $GLOBALS['log']->debug("In applyUserPreferences for ".$userObj->name);
 	    
@@ -48,13 +59,22 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	    }
 	}
 	
+	/**
+	 * Forces the relationship policy to the users.
+	 * Only one user can have a custom user preferences template.
+	 * This is called from the Users logic hook on after_relationship_add event.
+	 * 
+	 * @param object $userObj
+	 * @param string $event
+	 * @param array  $arguments
+	 */
 	public function forceRelationshipPolicy(&$userObj, $event, $arguments) {
 	    $relationship = $arguments['relationship'];
-	    $GLOBALS['log']->fatal('############################');
-	    $GLOBALS['log']->fatal('In forceRelationshipPolicy for '.$arguments['relationship']);
-	    $GLOBALS['log']->fatal('one time 1 '.$userObj->up_userpreferences_usersup_userpreferences_ida);
-	    $GLOBALS['log']->fatal('forced 1 '.$userObj->up_userpreferences_users_1up_userpreferences_ida);
-// 	    $GLOBALS['log']->fatal(print_r($userObj,true));
+	    $GLOBALS['log']->debug('############################');
+	    $GLOBALS['log']->debug('In forceRelationshipPolicy for '.$arguments['relationship']);
+	    $GLOBALS['log']->debug('one time 1 '.$userObj->up_userpreferences_usersup_userpreferences_ida);
+	    $GLOBALS['log']->debug('forced 1 '.$userObj->up_userpreferences_users_1up_userpreferences_ida);
+
 	    if (empty($userObj))
 	    {
 	        global $current_user;
@@ -63,67 +83,75 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	     
 	    //make sure user can only show under 1 subpanel under a Dashboard Template record
 	    $relationships = array(
-	        "up_userpreferences_users" => "up_userpreferences_users",
+	        "up_userpreferences_users"   => "up_userpreferences_users",
 	        "up_userpreferences_users_1" => "up_userpreferences_users_1",
 	    );
 	     
 	    if (!isset($relationships[$relationship]))
 	    {
-	        $GLOBALS['log']->fatal("relationship ".$relationship." not in array");
+	        $GLOBALS['log']->debug("relationship ".$relationship." not in array");
 	        return;
 	    }
 	     
 	    // one time user preferences
 	    if ($relationship == "up_userpreferences_users")
 	    {
-	        $GLOBALS['log']->fatal('one time user preferences '.$userObj->name);
-	        $GLOBALS['log']->fatal('one time 2 '.$userObj->up_userpreferences_usersup_userpreferences_ida);
+	        $GLOBALS['log']->debug('one time user preferences '.$userObj->name);
+	        $GLOBALS['log']->debug('one time 2 '.$userObj->up_userpreferences_usersup_userpreferences_ida);
 	        
 	        // if the user is in the one time user preferences, delete it
 	        if (!empty($userObj->up_userpreferences_usersup_userpreferences_ida))
 	        {
-	            $otherTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_usersup_userpreferences_ida);
-	            $otherTemplate->load_relationship("up_userpreferences_user");
-	            $otherTemplate->up_userpreferences_users->delete($otherTemplate->id, $userObj->id);
+	            $oneTimeTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_usersup_userpreferences_ida);
+	            $oneTimeTemplate->load_relationship("up_userpreferences_users");
+	            $oneTimeTemplate->up_userpreferences_users->delete($oneTimeTemplate->id, $userObj->id);
 	        }
-	        $GLOBALS['log']->fatal('forced 2 '.$userObj->up_userpreferences_users_1up_userpreferences_ida);
+	        $GLOBALS['log']->debug('forced 2 '.$userObj->up_userpreferences_users_1up_userpreferences_ida);
+	        
 	        // if the user is in the forced user preferences, delete it
 	        if (!empty($userObj->up_userpreferences_users_1up_userpreferences_ida))
 	        {
-	            $otherTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_users_1up_userpreferences_ida);
-	            $otherTemplate->load_relationship("up_userpreferences_users_1");
-	            $GLOBALS['log']->fatal("template id = ".$otherTemplate->id.", user id =".$userObj->id);
-	            $otherTemplate->up_userpreferences_users_1->delete($otherTemplate->id, $userObj->id);
+	            $forcedTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_users_1up_userpreferences_ida);
+	            $forcedTemplate->load_relationship("up_userpreferences_users_1");
+	            $GLOBALS['log']->debug("template id = ".$forcedTemplate->id.", user id =".$userObj->id);
+	            $forcedTemplate->up_userpreferences_users_1->delete($forcedTemplate->id, $userObj->id);
 	        }
 	    }
 	     
 	    // forced user preferences
 	    if ($relationship == "up_userpreferences_users_1")
 	    {
-	        $GLOBALS['log']->fatal('forced user preferences for '.$userObj->name);
-	        $GLOBALS['log']->fatal('forced 3 '.$userObj->up_userpreferences_users_1up_userpreferences_ida);
+	        $GLOBALS['log']->debug('forced user preferences for '.$userObj->name);
+	        $GLOBALS['log']->debug('forced 3 '.$userObj->up_userpreferences_users_1up_userpreferences_ida);
 	        
 	        // if the user is in the forced user preferences, delete it
 	        if (!empty($userObj->up_userpreferences_users_1up_userpreferences_ida))
 	        {
-	            $otherTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_users_1up_userpreferences_ida);
-	            $otherTemplate->load_relationship("up_userpreferences_users_1");
-	            $otherTemplate->up_userpreferences_users_1->delete($otherTemplate->id, $userObj->id);
+	            $forcedTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_users_1up_userpreferences_ida);
+	            $forcedTemplate->load_relationship("up_userpreferences_users_1");
+	            $forcedTemplate->up_userpreferences_users_1->delete($forcedTemplate->id, $userObj->id);
 	        }
-	        $GLOBALS['log']->fatal('one time 3 '.$userObj->up_userpreferences_usersup_userpreferences_ida);
+	        $GLOBALS['log']->debug('one time 3 '.$userObj->up_userpreferences_usersup_userpreferences_ida);
 	        
 	        // if the user is in the one time user preferences, delete it
 	        if (!empty($userObj->up_userpreferences_usersup_userpreferences_ida))
 	        {
 	            
-	            $otherTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_usersup_userpreferences_ida);
-	            $otherTemplate->load_relationship("up_userpreferences_user");
-	            $GLOBALS['log']->fatal("template id = ".$otherTemplate->id.", user id =".$userObj->id);
-	            $otherTemplate->up_userpreferences_users->delete($otherTemplate->id, $userObj->id);
+	            $oneTimeTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_usersup_userpreferences_ida);
+	            $oneTimeTemplate->load_relationship("up_userpreferences_users");
+	            $GLOBALS['log']->debug("template id = ".$oneTimeTemplate->id.", user id = ".$userObj->id);
+	            $GLOBALS['log']->debug(print_r($oneTimeTemplate->up_userpreferences_users, true));
+	            $oneTimeTemplate->up_userpreferences_users->delete($oneTimeTemplate->id, $userObj->id);
 	        }
 	    }
 	}
 	
+	/**
+	 * Retrieves the current user preferences for the current user
+	 * 
+	 * @param object $userObj
+	 * @return multitype:NULL
+	 */
 	public function getUserPreferences($userObj) {
 
 	    // forced user preferences
@@ -160,6 +188,12 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	    return $userPrefs;
 	}
 	
+	/**
+	 * Saves the current template user preferences
+	 * 
+	 * @param boolean $check_notify
+	 * @return integer
+	 */
 	public function save($check_notify = false) {
 	    
 	    // loop through the fields white list and update the current user preferences
@@ -185,6 +219,11 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	    return $id;
 	}
 	
+	/**
+	 * List of fields in the user preferences template
+	 * 
+	 * @return multitype:string
+	 */
 	protected function getFieldsList() {
 	    return array(
 	            'assigned_user_name', 'assigned_user_id',
@@ -197,6 +236,11 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	    );
 	}
 	
+	/**
+	 * List of enable flags field in the user preferences template
+	 * 
+	 * @return multitype:string
+	 */
 	protected function getEnableList() {
 	    return array(
 	        'date_format_enable', 'time_format_enable',
@@ -209,6 +253,11 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	    );
 	}
 	
+	/**
+	 * Returns the associative array between the enable flags and the field
+	 * 
+	 * @return multitype:string
+	 */
 	protected function getEnableFieldsList() {
 	    return array(
             'date_format_enable' => 'date_format', 
@@ -225,6 +274,11 @@ class up_UserPreferences extends up_UserPreferences_sugar {
         );
 	}
 	
+	/**
+	 * Returns the module field names and the actual user preferences array fields.
+	 * 
+	 * @return multitype:string
+	 */
 	protected function getObjectFieldList() {
 	    return array(
 	            'date_format' => 'datef',
--- installerpackage/SugarModules/modules/up_UserPreferences/up_UserPreferences.php
+++ installerpackage/SugarModules/modules/up_UserPreferences/up_UserPreferences.php
@@ -17,12 +17,23 @@
  */
 require_once 'modules/up_UserPreferences/up_UserPreferences_sugar.php' ;
 
+/**
+ * User Preferences Mass Update class
+ * @author dbergeron
+ *
+ */
 class up_UserPreferences extends up_UserPreferences_sugar {
 
 	public function __construct(){
 		parent::__construct();
 	}
 	
+	/**
+	 * Applies the current user preferences template to the current user logging in.
+	 * This method is called from the Users logic hook on after_login event
+	 * 
+	 * @param object $userObj
+	 */
 	public function applyUserPreferences($userObj) {
 	    $GLOBALS['log']->debug("In applyUserPreferences for ".$userObj->name);
 	    
@@ -48,6 +59,99 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	    }
 	}
 	
+	/**
+	 * Forces the relationship policy to the users.
+	 * Only one user can have a custom user preferences template.
+	 * This is called from the Users logic hook on after_relationship_add event.
+	 * 
+	 * @param object $userObj
+	 * @param string $event
+	 * @param array  $arguments
+	 */
+	public function forceRelationshipPolicy(&$userObj, $event, $arguments) {
+	    $relationship = $arguments['relationship'];
+	    $GLOBALS['log']->debug('############################');
+	    $GLOBALS['log']->debug('In forceRelationshipPolicy for '.$arguments['relationship']);
+	    $GLOBALS['log']->debug('one time 1 '.$userObj->up_userpreferences_usersup_userpreferences_ida);
+	    $GLOBALS['log']->debug('forced 1 '.$userObj->up_userpreferences_users_1up_userpreferences_ida);
+
+	    if (empty($userObj))
+	    {
+	        global $current_user;
+	        $userObj = $current_user;
+	    }
+	     
+	    //make sure user can only show under 1 subpanel under a Dashboard Template record
+	    $relationships = array(
+	        "up_userpreferences_users"   => "up_userpreferences_users",
+	        "up_userpreferences_users_1" => "up_userpreferences_users_1",
+	    );
+	     
+	    if (!isset($relationships[$relationship]))
+	    {
+	        $GLOBALS['log']->debug("relationship ".$relationship." not in array");
+	        return;
+	    }
+	     
+	    // one time user preferences
+	    if ($relationship == "up_userpreferences_users")
+	    {
+	        $GLOBALS['log']->debug('one time user preferences '.$userObj->name);
+	        $GLOBALS['log']->debug('one time 2 '.$userObj->up_userpreferences_usersup_userpreferences_ida);
+	        
+	        // if the user is in the one time user preferences, delete it
+	        if (!empty($userObj->up_userpreferences_usersup_userpreferences_ida))
+	        {
+	            $oneTimeTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_usersup_userpreferences_ida);
+	            $oneTimeTemplate->load_relationship("up_userpreferences_users");
+	            $oneTimeTemplate->up_userpreferences_users->delete($oneTimeTemplate->id, $userObj->id);
+	        }
+	        $GLOBALS['log']->debug('forced 2 '.$userObj->up_userpreferences_users_1up_userpreferences_ida);
+	        
+	        // if the user is in the forced user preferences, delete it
+	        if (!empty($userObj->up_userpreferences_users_1up_userpreferences_ida))
+	        {
+	            $forcedTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_users_1up_userpreferences_ida);
+	            $forcedTemplate->load_relationship("up_userpreferences_users_1");
+	            $GLOBALS['log']->debug("template id = ".$forcedTemplate->id.", user id =".$userObj->id);
+	            $forcedTemplate->up_userpreferences_users_1->delete($forcedTemplate->id, $userObj->id);
+	        }
+	    }
+	     
+	    // forced user preferences
+	    if ($relationship == "up_userpreferences_users_1")
+	    {
+	        $GLOBALS['log']->debug('forced user preferences for '.$userObj->name);
+	        $GLOBALS['log']->debug('forced 3 '.$userObj->up_userpreferences_users_1up_userpreferences_ida);
+	        
+	        // if the user is in the forced user preferences, delete it
+	        if (!empty($userObj->up_userpreferences_users_1up_userpreferences_ida))
+	        {
+	            $forcedTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_users_1up_userpreferences_ida);
+	            $forcedTemplate->load_relationship("up_userpreferences_users_1");
+	            $forcedTemplate->up_userpreferences_users_1->delete($forcedTemplate->id, $userObj->id);
+	        }
+	        $GLOBALS['log']->debug('one time 3 '.$userObj->up_userpreferences_usersup_userpreferences_ida);
+	        
+	        // if the user is in the one time user preferences, delete it
+	        if (!empty($userObj->up_userpreferences_usersup_userpreferences_ida))
+	        {
+	            
+	            $oneTimeTemplate = BeanFactory::getBean("up_UserPreferences", $userObj->up_userpreferences_usersup_userpreferences_ida);
+	            $oneTimeTemplate->load_relationship("up_userpreferences_users");
+	            $GLOBALS['log']->debug("template id = ".$oneTimeTemplate->id.", user id = ".$userObj->id);
+	            $GLOBALS['log']->debug(print_r($oneTimeTemplate->up_userpreferences_users, true));
+	            $oneTimeTemplate->up_userpreferences_users->delete($oneTimeTemplate->id, $userObj->id);
+	        }
+	    }
+	}
+	
+	/**
+	 * Retrieves the current user preferences for the current user
+	 * 
+	 * @param object $userObj
+	 * @return multitype:NULL
+	 */
 	public function getUserPreferences($userObj) {
 
 	    // forced user preferences
@@ -84,6 +188,12 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	    return $userPrefs;
 	}
 	
+	/**
+	 * Saves the current template user preferences
+	 * 
+	 * @param boolean $check_notify
+	 * @return integer
+	 */
 	public function save($check_notify = false) {
 	    
 	    // loop through the fields white list and update the current user preferences
@@ -109,6 +219,11 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	    return $id;
 	}
 	
+	/**
+	 * List of fields in the user preferences template
+	 * 
+	 * @return multitype:string
+	 */
 	protected function getFieldsList() {
 	    return array(
 	            'assigned_user_name', 'assigned_user_id',
@@ -121,6 +236,11 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	    );
 	}
 	
+	/**
+	 * List of enable flags field in the user preferences template
+	 * 
+	 * @return multitype:string
+	 */
 	protected function getEnableList() {
 	    return array(
 	        'date_format_enable', 'time_format_enable',
@@ -133,6 +253,11 @@ class up_UserPreferences extends up_UserPreferences_sugar {
 	    );
 	}
 	
+	/**
+	 * Returns the associative array between the enable flags and the field
+	 * 
+	 * @return multitype:string
+	 */
 	protected function getEnableFieldsList() {
 	    return array(
             'date_format_enable' => 'date_format', 
@@ -149,6 +274,11 @@ class up_UserPreferences extends up_UserPreferences_sugar {
         );
 	}
 	
+	/**
+	 * Returns the module field names and the actual user preferences array fields.
+	 * 
+	 * @return multitype:string
+	 */
 	protected function getObjectFieldList() {
 	    return array(
 	            'date_format' => 'datef',
